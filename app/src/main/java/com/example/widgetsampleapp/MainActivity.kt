9ec6284724469ee0fc7363ce0a0f2938c49c65e9
rmfgh2004 <rmfgh2004@naver.com>
2025-04-19 14:53:17 +0000

    study FrameLayout

--- app/src/main/java/com/example/widgetsampleapp/MainActivity.kt
+++ app/src/main/java/com/example/widgetsampleapp/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.layout_relative)
+        setContentView(R.layout.layout_frame)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
