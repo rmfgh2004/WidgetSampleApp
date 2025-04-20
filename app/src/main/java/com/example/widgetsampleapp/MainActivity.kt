e3be3fcb800650ea1e500774be53953157021ea4
rmfgh2004 <rmfgh2004@naver.com>
2025-04-20 11:08:33 +0000

    study text widget

--- app/src/main/java/com/example/widgetsampleapp/MainActivity.kt
+++ app/src/main/java/com/example/widgetsampleapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.widgetsampleapp
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +12,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.layout_constraint)
+        setContentView(R.layout.widget_text)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val textView = findViewById<AutoCompleteTextView>(R.id.tv_auto)
+        val countries: Array<String> = resources.getStringArray(R.array.countries_array)
+        ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, countries).also { adapter ->
+            textView.setAdapter(adapter);
         }
     }
 }
