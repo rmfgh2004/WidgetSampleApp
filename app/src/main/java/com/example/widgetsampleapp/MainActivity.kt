89bda0a0afc3a25e07f10870ad6502f9650f7a3c
rmfgh2004 <rmfgh2004@naver.com>
2025-04-20 14:36:48 +0000

    study listview

--- app/src/main/java/com/example/widgetsampleapp/MainActivity.kt
+++ app/src/main/java/com/example/widgetsampleapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.ListView
 import android.widget.NumberPicker
 import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
@@ -15,29 +16,38 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.widget_picker)
+        setContentView(R.layout.widget_list_view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-//        val textView = findViewById<AutoCompleteTextView>(R.id.tv_auto)
-//        val countries: Array<String> = resources.getStringArray(R.array.countries_array)
-//        ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, countries).also { adapter ->
-//            textView.setAdapter(adapter);
-//        }
+        setListViewWidget()
 
-//        val spinner = findViewById<Spinner>(R.id.sp_country)
-//        ArrayAdapter.createFromResource(
-//            this,
-//            R.array.countries_array,
-//            android.R.layout.simple_spinner_item
-//        ).also { adapter ->
-//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//            spinner.adapter = adapter
-//        }
+    }
 
+    private fun setTextWidget() {
+        val textView = findViewById<AutoCompleteTextView>(R.id.tv_auto)
+        val countries: Array<String> = resources.getStringArray(R.array.countries_array)
+        ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, countries).also { adapter ->
+            textView.setAdapter(adapter);
+        }
+    }
+
+    private fun setSelectWidget() {
+        val spinner = findViewById<Spinner>(R.id.sp_country)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.countries_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+    }
+
+    private fun setPickerWidget() {
         findViewById<Button>(R.id.btn_time).setOnClickListener {
             TimePickerFragment().show(supportFragmentManager, "timePicker")
         }
@@ -50,7 +60,16 @@ class MainActivity : AppCompatActivity() {
         numberPicker.maxValue = 100
         numberPicker.minValue = 0
         numberPicker.value = 5
+    }
 
+    private fun setListViewWidget() {
+        val listView = findViewById<ListView>(R.id.listView)
+        val itemList = mutableListOf<Int>()
+        for (i in 1..100) {
+            itemList.add(i)
+        }
 
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, itemList)
+        listView.adapter = adapter
     }
 }
