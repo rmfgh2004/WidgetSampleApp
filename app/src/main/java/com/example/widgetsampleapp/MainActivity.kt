70ecdcacfdc2c90ac76acd2947d44ae859ee8278
rmfgh2004 <rmfgh2004@naver.com>
2025-04-20 13:34:48 +0000

    study button widget

--- app/src/main/java/com/example/widgetsampleapp/MainActivity.kt
+++ app/src/main/java/com/example/widgetsampleapp/MainActivity.kt
@@ -12,17 +12,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.widget_text)
+        setContentView(R.layout.widget_button)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val textView = findViewById<AutoCompleteTextView>(R.id.tv_auto)
-        val countries: Array<String> = resources.getStringArray(R.array.countries_array)
-        ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, countries).also { adapter ->
-            textView.setAdapter(adapter);
-        }
+//        val textView = findViewById<AutoCompleteTextView>(R.id.tv_auto)
+//        val countries: Array<String> = resources.getStringArray(R.array.countries_array)
+//        ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, countries).also { adapter ->
+//            textView.setAdapter(adapter);
+//        }
     }
 }
